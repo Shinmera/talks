9748eaf731ba7eaa855b71f315764b5f33b26b55
Shinmera <shinmera@tymoon.eu>
2024-06-11 16:53:40 +0200

    bla

--- sgh2024-trial/presentation.lisp
+++ sgh2024-trial/presentation.lisp
@@ -2,7 +2,7 @@
 exec sbcl \
   --noinform \
   --disable-debugger \
-  --eval "(ql:quickload '(beamer) :silent T)" \
+  --eval "(ql:quickload '(beamer trial-glfw) :silent T)" \
   --eval "(beamer:start-slideshow \"$0\" :muffle-logging T)" \
   --quit \
   --end-toplevel-options "${@:1}"
@@ -28,8 +28,34 @@ exec sbcl \
   (items "Written entirely in Common Lisp"
          "Programming-focused engine"
          "PC Support, Nintendo Switch soon"
-         "Works well for 2D, 3D in the works"
          "Everything can be changed while the game is running"))
 
-(define-slide example
+(define-pool presentation)
+(define-asset (presentation player) model-file
+    #p "~/Projects/cl/weiss/data/models/weiss.glb")
+
+;; EDIT-1
+(define-shader-entity player (basic-animated-entity listener)
   ())
+
+(define-handler (player tick) (dt)
+  (let ((vel (vec 0 0 0)))
+    (when (retained :a) (incf (vx vel) 10.0))
+    (when (retained :d) (decf (vx vel) 10.0))
+    (fade-to (if (v/= vel 0) :running :idle) player)
+    (nv+ (tlocation (tf player)) (nv* vel dt))))
+;; EDIT-1
+
+(define-handler (player tick :after) (dt)
+  (handle tick (animation-controller player)))
+
+(define-slide example
+  (h "An example")
+  (editor "presentation.lisp"
+          :start ";; EDIT-1"
+          :end   ";; EDIT-1"
+          :language :lisp)
+  (enter-instance 'ambient-light :color (vec 0.5 0.5 0.5))
+  (enter-instance 'player :asset (asset 'presentation 'player))
+  (enter-instance 'target-camera :target (vec 0 2 0) :location (vec 0 3 5))
+  (enter-instance 'pbr-render-pass))
