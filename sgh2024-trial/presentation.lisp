5deb1220b7c1747a4b6ee55376f93593e8726fe9
Shinmera <shinmera@tymoon.eu>
2024-06-12 13:44:24 +0200

    Idk

--- sgh2024-trial/presentation.lisp
+++ sgh2024-trial/presentation.lisp
@@ -2,35 +2,17 @@
 exec sbcl \
   --noinform \
   --disable-debugger \
-  --eval "(ql:quickload '(beamer trial-glfw) :silent T)" \
+  --eval "(ql:quickload '(beamer trial-gltf) :silent T)" \
   --eval "(beamer:start-slideshow \"$0\" :muffle-logging T)" \
   --quit \
   --end-toplevel-options "${@:1}"
 |#
 
 (in-package #:beamer-user)
-
-(define-slide hello
-  (h "HELLO")
-  (image "avatar.png")
-  (p "Yukari Hafner")
-  (c "https://shinmera.com"))
-
-(define-slide me
-  (h "What I Do")
-  (items "Full-time indie"
-         "Open Source developer"
-         "Artist"
-         "etc"))
-
-(define-slide trial
-  (image "trial.png")
-  (items "Written entirely in Common Lisp"
-         "Programming-focused engine"
-         "PC Support, Nintendo Switch soon"
-         "Everything can be changed while the game is running"))
+(ql:quickload :trial-gltf)
 
 (define-pool presentation)
+
 (define-asset (presentation player) model-file
     #p "~/Projects/cl/weiss/data/models/weiss.glb")
 
@@ -49,13 +31,68 @@ exec sbcl \
 (define-handler (player tick :after) (dt)
   (handle tick (animation-controller player)))
 
+(define-slide zero)
+
+(define-slide hello
+  (image "avatar.png" '(500 300))
+  (h "HELLO" :size 80 :halign :center)
+  (p "I'm Yukari Hafner" :size 48 :halign :center)
+  (c "https://shinmera.com" :halign :center))
+
+(define-slide me
+  (h "What I Do")
+  (items "Full-time indie"
+         "Open Source developer"
+         "Artist"
+         "etc"))
+
+(define-slide trial
+  (image "trial.png" '(500 250) :padding (alloy:margins 50))
+  (items "Written entirely in Common Lisp"
+         "Programming-focused engine"
+         "PC Support, Nintendo Switch soon"
+         "Everything can be changed while the game is running"))
+
 (define-slide example
   (h "An example")
-  (editor "presentation.lisp"
-          :start ";; EDIT-1"
-          :end   ";; EDIT-1"
-          :language :lisp)
+  (editor "presentation.lisp" :start ";; EDIT-1" :end ";; EDIT-1" :language :lisp)
   (enter-instance 'ambient-light :color (vec 0.5 0.5 0.5))
   (enter-instance 'player :asset (asset 'presentation 'player))
   (enter-instance 'target-camera :target (vec 0 2 0) :location (vec 0 3 5))
   (enter-instance 'pbr-render-pass))
+
+(define-slide runtime
+  (h "So what can be changed at runtime?")
+  (p "Everything!")
+  (items "Variables"
+         "Assets"
+         "Shaders"
+         "Classes"
+         "Methods"
+         "What class an object is an instance of!"))
+
+(define-slide performance
+  (h "Performance")
+  (items "Lisp code is compiled to machine code"
+         "Can give the compiler lots of hints on how to optimise"
+         "Can even customise the compiler itself if need-be"
+         "All while remaining interactive"))
+
+(define-slide features
+  (h "Trial Features")
+  (items "PBR rendering"
+         "3D physics system"
+         "Integration with Steam, Blender, Aseprite"
+         "Broad image/audio format support"
+         "Automated release management"
+         "Automated crash reports"))
+
+(define-slide learn-more
+  (h "Where to Learn More?")
+  (items "Learning Lisp: https://gigamonkeys.com/book"
+         "Learning Trial: https://shirakumo.org/docs/trial"
+         "Talk to me!"))
+
+(define-slide buy
+  (image "kandria.png" '(1920 500))
+  (c "https://kandria.com" :halign :center :size 50))
